6e3dba4d8c70af0feb26ee021d3601a7060ce35c
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-09-20 19:52:26 -0400

    updated icicles to actually cover width

--- src/components/Icicles/Icicle.tsx
+++ src/components/Icicles/Icicle.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import { IcicleStyle } from './IciclesStyles';
 
 interface IcicleProps {
-	values: { height: number; sides: number };
+	values: { height: number; width: number };
 }
 
 const Icicle: React.FC<IcicleProps> = ({ values }) => {
-	const generateClip = (max: number, min: number) =>
-		Math.floor(Math.random() * (max - min + 1) + min);
-
 	return (
 		<>
 			<IcicleStyle
 				style={{
-					width: values.sides,
+					width: values.width,
 					height: values.height,
-					clipPath: `polygon(${generateClip(20, 0)} 0, 50% 100%, ${generateClip(
-						100,
-						80,
-					)}% 0)`,
 				}}
 			/>
 		</>
--- src/components/Icicles/index.tsx
+++ src/components/Icicles/index.tsx
@@ -5,39 +5,39 @@ import { IcicleWrapper } from './IciclesStyles';
 interface IciclesProps {
 	parentRef: React.MutableRefObject<HTMLElement>;
 	heightConstrains?: { min: number; max: number };
-	sideConstrains?: { min: number; max: number };
+	widthConstrains?: { min: number; max: number };
 }
 
 const Icicles: React.FC<IciclesProps> = ({
 	parentRef,
 	heightConstrains = { min: 30, max: 60 },
-	sideConstrains = { min: 10, max: 40 },
+	widthConstrains = { min: 10, max: 40 },
 }) => {
 	const getParentWidth = () => parentRef.current?.offsetWidth;
-	const [icicles, setIcicles] = useState<{ height: number; sides: number }[]>([]);
+	const [icicles, setIcicles] = useState<{ height: number; width: number }[]>([]);
 
 	const createIcicles = () => {
 		const max = getParentWidth();
 		let curr = 0;
-		const icicleValues: { height: number; sides: number }[] = [];
+		const icicleValues: { height: number; width: number }[] = [];
 		while (true) {
 			if (curr < max) {
 				const height = Math.floor(
 					Math.random() * (heightConstrains.max - heightConstrains.min + 1) +
 						heightConstrains.min,
 				);
-				const sides = Math.floor(
-					Math.random() * (sideConstrains.max - sideConstrains.min + 1) +
-						sideConstrains.min,
+				const width = Math.floor(
+					Math.random() * (widthConstrains.max - widthConstrains.min + 1) +
+						widthConstrains.min,
 				);
 				if (curr + height > max) {
-					const shortenedHeight = max - curr;
-					if (shortenedHeight < 20) break;
-					icicleValues.push({ height: shortenedHeight, sides: sides });
+					const shortenedwidth = max - curr;
+					if (shortenedwidth < 20) break;
+					icicleValues.push({ height: height, width: shortenedwidth });
 					break;
 				}
-				icicleValues.push({ height: height, sides: sides });
-				curr += height;
+				icicleValues.push({ height: height, width: width });
+				curr += width;
 			} else {
 				break;
 			}
