77b93f51c4fd56538feae34792768230e709462c
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-09-23 13:58:31 -0400

    removed unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 import Layout from '../components/Layout';
-import { graphql } from 'gatsby';
 import EventsDisplay from '../components/EventsDisplay';
 
 const Home: React.FC<PageProps> = () => {
