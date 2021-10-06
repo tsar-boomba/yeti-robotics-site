8548f51847f21a3555589702630ed492ee42d47f
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-10-05 21:51:59 -0400

    edited section styles

--- src/components/Section/SectionStyles.ts
+++ src/components/Section/SectionStyles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const SectionStyles = styled.section`
 	width: 100%;
-	margin: 20px 0 0 0;
+	margin: 2rem 0 0 0;
 	border: 10px solid #000000;
 	border-right: 0;
 	border-left: 0;
-	display: grid;
-	place-items: center;
-	text-align: center;
+	display: flex;
+	flex-flow: column;
+	align-items: center;
 `;
